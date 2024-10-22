0194eeb38d338b82afbdc0c46415d3930a43b123
Van Linh <linh.nguyenvan@phenikaa-uni.edu.vn>
2024-10-22 18:54:39 +0700

    Revert to old version build

--- src/foundation-gtk+/version.mk
+++ src/foundation-gtk+/version.mk
@@ -1,8 +1,8 @@
 NAME		= foundation-gtk+
 PKGROOT		= /opt/rocks
-VERSION		= 3.22.30
+VERSION		= 3.16.7
 RELEASE		= 0
 SUBDIR		= gtk+-$(VERSION)
 TARFILE		= $(SUBDIR).tar.xz
-RPM.REQUIRES	= foundation-atk,foundation-at-spi2-atk,foundation-glib2
+RPM.REQUIRES	= foundation-atk,foundation-at-spi2-atk,foundation-glib2,foundation-devel-module
 RPM.FILES	= "$(PKGROOT)/bin/*\\n$(PKGROOT)/etc/*\\n$(PKGROOT)/include/*\\n$(PKGROOT)/lib/*\\n$(PKGROOT)/share/*"
