8431f7bc06aa45a5e5172052fc5d4bc28585aa2f
Van Linh <Ljnk975@gmail.com>
2024-10-22 22:38:40 +0700

    Move pango to kernel

---
+++ src/foundation-pango/version.mk
@@ -0,0 +1,8 @@
+NAME	   = foundation-pango
+PKGROOT    = /opt/rocks
+VERSION    = 1.42.4
+RELEASE    = 8
+RPM.FILES  = "$(PKGROOT)/bin/*\\n$(PKGROOT)/include/*\\n$(PKGROOT)/lib/*\\n$(PKGROOT)/share/*"
+SRCPKGNAME = pango-1.42.4-8.el8.src.rpm
+SRCPKGHOST = https://download.rockylinux.org
+SRCPKGPATH = pub/rocky/8/AppStream/source/tree/Packages/p
