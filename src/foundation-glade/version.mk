5470e46b5d14437b69f4dce14346d82ea3fe8e37
Van Linh <linh.nguyenvan@phenikaa-uni.edu.vn>
2024-10-22 19:28:26 +0700

    Fix build alma

--- src/foundation-glade/version.mk
+++ src/foundation-glade/version.mk
@@ -4,5 +4,5 @@ VERSION		= 3.19.0
 RELEASE		= 0
 SUBDIR		= glade-$(VERSION)
 TARFILE		= $(SUBDIR).tar.xz
-RPM.REQUIRES	= foundation-atk,foundation-at-spi2-atk,foundation-glib2,foundation-gtk+,foundation-devel-module
+RPM.REQUIRES	= foundation-atk,foundation-at-spi2-core,foundation-at-spi2-atk,foundation-glib2,foundation-gtk+,foundation-devel-module
 RPM.FILES	= "$(PKGROOT)/bin/*\\n$(PKGROOT)/include/*\\n$(PKGROOT)/lib/*\\n$(PKGROOT)/share/*"
--- src/foundation-gtk+/version.mk
+++ src/foundation-gtk+/version.mk
@@ -4,5 +4,5 @@ VERSION		= 3.16.7
 RELEASE		= 0
 SUBDIR		= gtk+-$(VERSION)
 TARFILE		= $(SUBDIR).tar.xz
-RPM.REQUIRES	= foundation-atk,foundation-at-spi2-atk,foundation-glib2,foundation-devel-module
+RPM.REQUIRES	= foundation-atk,foundation-at-spi2-core,foundation-at-spi2-atk,foundation-glib2,foundation-devel-module
 RPM.FILES	= "$(PKGROOT)/bin/*\\n$(PKGROOT)/etc/*\\n$(PKGROOT)/include/*\\n$(PKGROOT)/lib/*\\n$(PKGROOT)/share/*"
